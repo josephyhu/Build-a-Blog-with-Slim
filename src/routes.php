995cd6a150bba07e8740906f9006c96764173426
Joseph Yhu <xbhlx7@gmail.com>
2019-09-06 23:45:34 -0400

    Add files

---
+++ src/routes.php
@@ -0,0 +1,24 @@
+<?php
+// Routes
+
+$app->map(['GET', 'POST'], '/new', function ($request, $response, $args) {
+    return $this->view->render($response, 'new.phtml', $args);
+});
+
+$app->post('/edit', function ($request, $response, $args) {
+    return $this->view->render($response, 'edit.phtml', $args);
+});
+
+$app->get('/entries', function ($request, $response, $args) {
+
+    // Render details view
+    return $this->renderer->render($response, 'detail.phtml', $args);
+});
+
+$app->get('/', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
