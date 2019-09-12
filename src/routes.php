4f94ab916cde3fb3126a6ea5e2b7e0ead745dd03
Joseph Yhu <xbhlx7@gmail.com>
2019-09-12 12:31:06 -0400

    Update

--- src/routes.php
+++ src/routes.php
@@ -6,24 +6,24 @@ $app->get('/new', function ($request, $response, $args) {
     return $this->view->render($response, 'new.phtml', $args);
 });
 
-$app->get('/delete/[{id}]', function ($request, $response, $args) {
+$app->get('/delete/{id}', function ($request, $response, $args) {
     $args['post'] = $this->post;
     return $this->view->render($response, 'delete.phtml', $args);
 });
 
-$app->get('/edit/[{id}]', function ($request, $response, $args) {
+$app->get('/edit/{id}', function ($request, $response, $args) {
     $args['post'] = $this->post;
     return $this->view->render($response, 'edit.phtml', $args);
 });
 
-$app->get('/entries/[{title}]', function ($request, $response, $args) {
+$app->get('/entries/{title}', function ($request, $response, $args) {
 
     // Render details view
     $args['post'] = $this->post;
     return $this->renderer->render($response, 'detail.phtml', $args);
 });
 
-$app->get('/tags/[{tag}]', function ($request, $response, $args) {
+$app->get('/tags/{tag}', function ($request, $response, $args) {
     $args['post'] = $this->post;
     return $this->renderer->render($response, 'tags.phtml', $args);
 });
